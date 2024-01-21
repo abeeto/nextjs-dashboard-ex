d75112723f76859474d96ce244972eda10575320
Abhinav Thota <abhinavthota01@gmail.com>
2024-01-20 16:22:34 -0800

    Added shared layout for all the pages under dashboard. Very eye opening. Nice...

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return (<p className="bg-teal-800 md:text-xl text-white p-7">Customers</p>)
+}
---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <p className="bg-teal-900 p-7 md:text-xl text-white">Dashboard</p>;
+}
