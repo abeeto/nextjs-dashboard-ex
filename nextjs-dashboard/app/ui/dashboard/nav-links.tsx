a6cf30c3de6638f922aa0a35181de016f9ca524a
Abhinav Thota <abhinavthota01@gmail.com>
2024-01-20 22:46:10 -0800

    mind blown, learnt how to set up links and use clsx to style elements conditionally

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,11 +1,16 @@
+"use client"
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
+// look at how beautifully they stored the data for the links by assinging it to the object const links. *chef's kiss*
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
@@ -17,19 +22,26 @@ const links = [
 ];
 
 export default function NavLinks() {
+  // when you use <a> tags, the links cause browsers to do a full refresh to open page. no no. can be faster/better to use Links.
+  const thisPath = usePathname();
   return (
     <>
-      {links.map((link) => {
+      {
+      links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            ,{
+              'bg-sky-100 text-blue-600': thisPath === link.href,
+            },)}
+          > 
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
